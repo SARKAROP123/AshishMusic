e52cbd6adaccdaec71b47d998ee616a2beed3c1f
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-06-22 13:29:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -74,7 +74,7 @@ STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
 
-BANNED_USERS = filters.user(Bwf)
+BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
 votemode = {}
